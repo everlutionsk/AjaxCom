a5829c718c62e5d08bfd66f41a10d7fbdf16530f
Miroslav Demovic <demovic@advertize.sk>
2013-01-06 17:07:08 +0000

    Temporary Modal files

--- src/DM/AjaxCom/Responder/Modal.php
+++ src/DM/AjaxCom/Responder/Modal.php
@@ -6,16 +6,53 @@ class Modal extends ResponseObjectAbstract
 {
     const OBJECT_IDENTIFIER = 'modal';
 
-    const OPTION_WIDTH = 'width';
+    private $title;
+    
+    private $width;
+    private $height;
+    
+
 
     /**
      * Constructor
      *
      * @param string $function
      */
-    public function __construct($function)
+    public function __construct()
     {
-        $this->registerOption(self::OPTION_FUNCTION);
-        $this->setOption(self::OPTION_FUNCTION, $function);
+#       $this->registerOption(self::OPTION_FUNCTION);
+#       $this->setOption(self::OPTION_FUNCTION, $function);
+        return $this;
+    }
+
+    public function __call($method, $args) 
+    {
+        if (preg_match('#^(set|get)([a-zA-Z_]+)$#', $method, $matches)) {
+            $property = strtolower($matches[2]);
+
+            if (!property_exists($this, $property)) {
+                throw new \Exception('Property '.$property.' cannot be set');
+            }
+            switch($matches[1]) {
+                case 'set':
+                    return $this->set($property, $args[0]);
+                case 'get':
+                    return $this->get($property);
+            }
+        }
+    }
+    
+    public function get($property) {
+        return $this->$property;
+    }
+
+    public function set($property, $value) {
+        $this->$property = $value;
+        return $this;
+    }
+
+    public function render()
+    {
+        return "xxssssxx".$this->getTitle();    
     }
 }
