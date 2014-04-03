d9273f0de40551548ffc376490c4d4df9d38a414
Miro Demovic <miroslav.demovic@everlution.com>
2014-04-03 15:36:32 +0200

    #14 - json code bug

--- src/DM/AjaxCom/Resources/public/js/ajaxcom.js
+++ src/DM/AjaxCom/Resources/public/js/ajaxcom.js
@@ -56,7 +56,7 @@
                 }
 
                 xhr.setRequestHeader('X-AjaxCom', 'true');
-                xhr.setRequestHeader('Accept', 'application/json');
+                xhr.setRequestHeader('Accept': 'Vary');
             },
             success : function(data, status, xhr){
                 //Running external definition
